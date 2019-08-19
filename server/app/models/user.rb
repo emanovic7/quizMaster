ff2be35823ad1877fc37e4bb0a87ed6d35cc918a
github username <github email address>
2019-08-19 17:49:52 -0400

    add quiz

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+    has_many :quizzes
+    
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_205908) do
+ActiveRecord::Schema.define(version: 2019_08_19_214808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string "category"
+    t.string "difficulty"
+    t.string "type"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
