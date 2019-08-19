cb7eb6a2b1ff22cad2d36ae46c706b80ba0997af
github username <github email address>
2019-08-19 17:22:02 -0400

    add practice API fetch to main.js

---
+++ quizMaster-Frontend/main.js
@@ -0,0 +1,23 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  fetch("https://opentdb.com/api.php?amount=10")
+  .then(response => response.json())
+  .then(data => slapQuizzesOnDom(data))
+  //.then(data => console.log(data.results))
+})
+
+
+function slapQuizzesOnDom(data){
+  const ul = document.getElementById("quizzes-ul");
+
+  data.results.forEach(function(quiz){
+    let li = document.createElement("li");
+    li.innerHTML = `
+      <p>${quiz.question}</p>
+    `
+
+    ul.append(li)
+  })
+}
