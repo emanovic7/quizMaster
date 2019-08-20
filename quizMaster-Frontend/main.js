1df9a4018e6e5de45b71b7692a0ce6fc80d0a2bc
emanovic7 <e_owusu@live.com>
2019-08-20 15:59:15 -0400

    add frontend fetch to main.js

--- quizMaster-Frontend/main.js
+++ quizMaster-Frontend/main.js
@@ -1,15 +1,72 @@
 
 
 document.addEventListener("DOMContentLoaded", function(){
-const loginForm = document.getElementById("login-form")
-
- 
+  const loginForm = document.getElementById("login-form");
 })
-  fetch("https://opentdb.com/api.php?amount=10")
-  .then(response => response.json())
-  .then(data => slapQuizzesOnDom(data))
-  //.then(data => console.log(data.results))
 
+
+//LOGGING IN
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", loginUser)
+
+
+function loginUser(){
+  event.preventDefault();
+  let name = event.target.querySelector("#username").value;
+  if(name === ""){
+    alert("Username can't be empty!")
+  }
+  else{
+    const loginForm = document.getElementById("login-form");
+    loginForm.remove();
+    containers();
+  }
+}
+
+
+function containers(){
+  const optionsDiv = document.getElementById("main_options");
+  let heading = document.createElement("h2")
+  heading.innerHTML = `<h2>Welcome, choose an option below</h2>`
+  optionsDiv.append(heading)
+}
+
+
+
+
+
+
+
+
+
+function fetchUsers(){
+  fetch("http://localhost:3000/users")
+  .then(response => response.json())
+  .then(mainOptionsDiv)
+}
+
+//MAIN PAGE OPTIONS
+function mainOptions(){
+  let startQuiz = document.createElement("div");
+  div.className = "main_options";
+  mainOptionsDiv.append(div)
+}
+
+
+
+
+
+
+
+
+
+
+//FETCH QUIZZES
+function fetchQuizzes(){
+  fetch("http://localhost:3000/quizzes")
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
 
 
 function slapQuizzesOnDom(data){
@@ -24,3 +81,8 @@ function slapQuizzesOnDom(data){
     ul.append(li)
   })
 }
+
+
+
+
+//
